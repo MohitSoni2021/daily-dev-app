db522f869f84f9a440ed634770efeacc4ad99ee0
mohitsoni2024 <mohitsoni2006ms@gmail.com>
2024-12-02 11:12:30 +0530

    login check middleware to page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,16 @@
+'use client'
+
 import React from 'react'
 import HomePageLayout from './components/HomePage/HomePageLayout'
+import { redirect } from 'next/navigation'
+
+
+const loginCheckMiddleware = ()=>{
+  if(localStorage.getItem('logined_user_email_of_daily_dev_copy_app')==null){
+    redirect('/login')
+  }
+}
+loginCheckMiddleware();
 
 const MainPage = () => {
   return (
