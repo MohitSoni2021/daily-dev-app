7d68768ac2ce561cdfb1b6545a07fa66c29668dd
mohitsoni2024 <mohitsoni2006ms@gmail.com>
2024-12-03 09:50:07 +0530

    import-updates

--- app/api/v1/home_card_data/route.tsx
+++ app/api/v1/home_card_data/route.tsx
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse } from 'next/server'
+import { NextResponse } from 'next/server'
 import { home_card_data } from '../../../sample-database/feed-data'
 
 export async function GET() {
