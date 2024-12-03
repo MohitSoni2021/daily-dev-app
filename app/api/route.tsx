e60a65271108fce17cb42aee00eb2f3a24141c73
mohitsoni2024 <mohitsoni2006ms@gmail.com>
2024-12-03 09:43:06 +0530

    eslint rule changement

--- app/api/route.tsx
+++ app/api/route.tsx
@@ -3,7 +3,7 @@ import { home_card_data } from '../sample-database/feed-data'
 
 export async function GET(request: NextRequest) {
 
-  const url = request.nextUrl
+
   return NextResponse.json({
     status: 200,
     data: "This is the data of the second route request.",
