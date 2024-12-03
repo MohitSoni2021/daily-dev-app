a1ec5439ca04e39815b2ebec949ce3c28a0e7f75
mohitsoni2024 <mohitsoni2006ms@gmail.com>
2024-12-03 09:46:49 +0530

    api-fixes

--- app/api/route.tsx
+++ app/api/route.tsx
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { home_card_data } from '../sample-database/feed-data'
 
-export async function GET(request: NextRequest) {
+export async function GET() {
 
 
   return NextResponse.json({
